b230480eedad1d9cc10fb4cf770cca807ef7cf44
Tabby Douglass <tabbytoney@gmail.com>
2022-03-13 14:25:12 -0500

    Apply Layout styling component to all pages in app.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -5,6 +5,7 @@ function Layout(props) {
   return (
     <div>
       <MainNavigation />
+      {/* uses props.children to take what's between Layout tags and wrap this component around this content */}
       <main className={classes.main}>{props.children}</main>
     </div>
   );
