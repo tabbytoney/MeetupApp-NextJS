b6b0182fcfb320a5926bd53794d30fa0bf6ec692
Tabby Douglass <tabbytoney@gmail.com>
2022-03-14 17:26:22 -0500

    Use router.push in a handler for detail pages with different urls

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,15 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import { useRouter } from 'next/router';
 
 function MeetupItem(props) {
+  const router = useRouter();
+  function showDetailsHandler() {
+    // .push pushes a new page onto the stack of pages and is the equivalent of using Link component
+    // It takes a path as its parameter
+    // props.id gets meetup.id in MeetupItem
+    router.push('/' + props.id);
+  }
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +21,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
